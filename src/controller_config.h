61100712987a6dcf12cb0ac4882f3d5404c3ab6c
tenebraeq <69158882+tenebraeq@users.noreply.github.com>
2024-12-17 16:31:57 +0500

    Update controller_config.h

--- src/controller_config.h
+++ src/controller_config.h
@@ -1,8 +1,8 @@
 #ifndef CONTROLLER_CONFIG_H
 #define CONTROLLER_CONFIG_H
 
-#define SW_GPIO_SIZE 11               // Number of switches
-#define LED_GPIO_SIZE 10              // Number of switch LEDs
+#define SW_GPIO_SIZE 9               // Number of switches
+#define LED_GPIO_SIZE 9              // Number of switch LEDs
 #define ENC_GPIO_SIZE 2               // Number of encoders
 #define ENC_PPR 600                   // Encoder PPR
 #define MOUSE_SENS 1                  // Mouse sensitivity multiplier
@@ -18,18 +18,18 @@
 #ifdef PICO_GAME_CONTROLLER_C
 
 // MODIFY KEYBINDS HERE, MAKE SURE LENGTHS MATCH SW_GPIO_SIZE
-const uint8_t SW_KEYCODE[] = {HID_KEY_D, HID_KEY_F, HID_KEY_J, HID_KEY_K,
-                              HID_KEY_C, HID_KEY_M, HID_KEY_A, HID_KEY_B,
-                              HID_KEY_1, HID_KEY_E, HID_KEY_G};
+const uint8_t SW_KEYCODE[] = {HID_KEY_M, HID_KEY_K, HID_KEY_J,
+                              HID_KEY_1, HID_KEY_F, HID_KEY_D,
+                              HID_KEY_C, HID_KEY_A, HID_KEY_B};
 const uint8_t SW_GPIO[] = {
-    4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 27,
+    1, 3, 5, 7, 9, 11, 13, 15, 16, 26,
 };
 const uint8_t LED_GPIO[] = {
-    5, 7, 9, 11, 13, 15, 17, 19, 21, 26,
+    0, 2, 4, 6, 8, 10, 12, 14, 17, 27,
 };
-const uint8_t ENC_GPIO[] = {0, 2};      // L_ENC(0, 1); R_ENC(2, 3)
+const uint8_t ENC_GPIO[] = {18, 21};      // L_ENC(0, 1); R_ENC(2, 3)
 const bool ENC_REV[] = {false, false};  // Reverse Encoders
-const uint8_t WS2812B_GPIO = 28;
+const uint8_t WS2812B_GPIO = 20;
 
 #endif
 
